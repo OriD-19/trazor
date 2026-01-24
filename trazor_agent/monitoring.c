756edf31296fbb60c0e88c0f32ef6eb996632a46
Fernando Fuentes <fernandocastillo.8@hotmail.com>
2026-01-23 22:47:43 -0600

    adding tentative queue-type map

--- trazor_agent/monitoring.c
+++ trazor_agent/monitoring.c
@@ -3,6 +3,16 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 
+struct connection_info {
+    int port;
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_QUEUE);
+    __type(value, struct connection_info);
+    __uint(max_entries, 500);
+} sock_info SEC(".maps");
+
 struct trace_event_raw_sys_enter_connect {
     u64 __unused__;
     u32 _syscall_nr;
@@ -12,7 +22,14 @@ struct trace_event_raw_sys_enter_connect {
 };
 
 SEC("tp/syscalls/sys_enter_connect")
-int fernandito(struct trace_event_raw_sys_enter_connect *ctx) {
+int collect_enter_traces(struct trace_event_raw_sys_enter_connect *ctx) {
+    
+    struct sockaddr_in addr;
+
+    if (ctx->uservaddr != NULL) {
+        bpf_probe_read_user(&addr, sizeof(addr), ctx->uservaddr); // addr recieves the data
+    }
+
     return 0;
 }
 
