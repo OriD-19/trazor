cc60b074955dd448e38b65613e5194b3f88e74b2
Fernando Fuentes <fernandocastillo.8@hotmail.com>
2026-01-24 21:53:50 -0600

    wip: first iteration of the program

---
+++ trazor_agent/main.go
@@ -0,0 +1,50 @@
+package main
+//go:generate go tool bpf2go -tags linux trazor_agent monitoring.c
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/ringbuf"
+	"github.com/cilium/ebpf/rlimit"
+)
+
+func main() {
+	// boilerplate code
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal("Removing Memlock: ", err)
+	}
+
+	var objs trazor_agentObjects
+	if err := loadTrazor_agentObjects(&objs, nil); err != nil {
+		log.Fatal("Loading eBPF objects: ", err)
+	}
+	defer objs.Close()
+
+	// attach the programs to their respective uprobes
+	link.Kprobe("get_conn_start", objs.GetConnStart, nil)
+	link.Kprobe("get_latency_on_end", objs.GetLatencyOnEnd, nil)
+
+	ringBuf, err := ringbuf.NewReader(objs.Events)
+	if err != nil {
+		log.Fatal("Opening ringbuf reader: ", err)	
+		os.Exit(1);
+	}
+
+	go func() {
+		for {
+			record, err := ringBuf.Read()
+
+			if err != nil {
+				log.Fatal("Reading ringbuf: ", err)
+			}
+
+			fmt.Println("Event: ", string(record.RawSample))
+
+			if err := ringBuf.Close(); err != nil {
+				log.Fatal("Closing ringbuf reader: ", err)
+			}
+		}
+	}()
+}
--- trazor_agent/monitoring.c
+++ trazor_agent/monitoring.c
@@ -3,6 +3,8 @@
 #include <bpf/bpf_helpers.h>
 
 struct http_event {
+    __u32 __padding__;
+
     __u64 timestamp;
     __u64 latency_ns;
     __u32 pid;
@@ -12,7 +14,7 @@ struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __type(key, __u32);
     __type(value, __u64);
-    __uint(max_entries, 256 * 1024);
+    __uint(max_entries, 192 * 1024);
 } latency SEC(".maps");
 
 struct {
@@ -33,22 +35,21 @@ int get_conn_start(struct pt_regs *ctx) {
 
 SEC("uprobe/ngx_http_finalize_connection")
 int get_latency_on_end(struct pt_regs *ctx) {
-
     struct http_event *req_info;
     u64 pid = bpf_get_current_pid_tgid() >> 32;
     u64 ts = bpf_ktime_get_ns();
 
     // last value is always 0, for some reason...
-    req_info = bpf_ringbuf_reserve(&req_info, sizeof(*req_info), 0);
+    req_info = bpf_ringbuf_reserve(&events, sizeof(*req_info), 0);
     if (!req_info) // no valid memory allocated, returned NULL
         return 0;
 
     req_info->timestamp = ts;
 
     // get start time of this request 
-    u64 init = *(u64*)bpf_map_lookup_elem(&latency, &pid);
+    u64 *init = bpf_map_lookup_elem(&latency, &pid);
 
-    u64 delta = ts - init;
+    u64 delta = ts - *init;
 
     req_info->latency_ns = delta;
     req_info->pid = pid;
